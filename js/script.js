654ef6b1d208675fd3227790e4a55b606f7ae2c1
tomo-2020 <shige5th2008@yahoo.co.jp>
2020-11-02 14:50:21 +0900

    ＜追加・修正＞全てのセクションに対してタブレット用、スマホ用にコンテンツを追加・修正

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
 jQuery(document).ready(function () {
     // スティッキーヘッダー
-    $(function () {
-        var $win = $(window),
-            $hero = $('.hero'),
-            $header = $('.header'),
-            heroHeight = $hero.outerHeight();
-        fixedClass = 'js-fixed';
+    // $(function () {
+    //     var $win = $(window),
+    //         $hero = $('.hero'),
+    //         $header = $('.header'),
+    //         heroHeight = $hero.outerHeight();
+    //     fixedClass = 'js-fixed';
 
-        $win.on('load scroll', function () {
-            var value = $(this).scrollTop();
-            if ($win.width() > 768) {
-                if (value > heroHeight) {
-                    $header.addClass(fixedClass);
-                } else {
-                    $header.removeClass(fixedClass);
-                }
-            }
-        });
-    });
+    //     $win.on('load scroll', function () {
+    //         var value = $(this).scrollTop();
+    //         if ($win.width() > 768) {
+    //             if (value > heroHeight) {
+    //                 $header.addClass(fixedClass);
+    //             } else {
+    //                 $header.removeClass(fixedClass);
+    //             }
+    //         }
+    //     });
+    // });
 
     // ハンバーガーメニュー
     $(function () {
+        //右横からフェードイン
         $('.hamburger-bottun').on('click', function () {
             $('.hamburger-bottun').toggleClass('js-hamburger-bottun--close');
-            $('.header__sp-navi-wrapper').fadeToggle(500);
-            $('.body').toggleClass('js-body--noscroll'); // ナビ表示時のスクロールを防ぐ
+            $('.header__sp-navi-wrapper').toggleClass('slide-in');
+            $('.body').toggleClass('js-body--noscroll');
             $('.header__sp-navi a[href]').off('click'); // ボタンクリックの繰り返しを防ぐ
             $('.header__sp-navi a[href]').on('click', function (event) {
                 // ナビをクリックした際にナビが閉じる
@@ -32,18 +33,6 @@ jQuery(document).ready(function () {
             });
         });
     });
-
-    // slickスライダー hero部分
-    // $(function () {
-    //     $('.js-hero-slick').slick({
-    //         autoplay: true, // 自動再生
-    //         autoplaySpeed: 2000, // 画像を表示している時間
-    //         speed: 1000, // 切り替え時の時間
-    //         fade: true, // 画像の切り替えをフェードイン・フェードアウトにさせる
-    //         cssEase: 'linear', // 画像切り替え時のイージングを CSS3 Animation Easing で指定
-    //         dots: true, // ドットインジケータの表示
-    //     });
-    // });
 
     // スクロールトップ
     $(function () {
