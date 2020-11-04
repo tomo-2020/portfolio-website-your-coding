c92acf1170d5e1500b2e8b7c72baedf1688ea27b
tomo-2020 <shige5th2008@yahoo.co.jp>
2020-11-04 12:05:32 +0900

    ＜修正＞jsファイルの読み込み方法を変更
    機能別に分けて読み込んでいいたjavascriptの各ファイルを、import.jsファイルに集約しました。

---
+++ js/import.js
@@ -0,0 +1,7 @@
+document.write('<script type="text/javascript" src="js/accordion.js"></script>');
+document.write('<script type="text/javascript" src="js/hamburger.js"></script>');
+document.write('<script type="text/javascript" src="js/scroll-top.js"></script>');
+document.write('<script type="text/javascript" src="js/smooth-scroll.js"></script>');
+// document.write('<script type="text/javascript" src="js/sticky-header.js"></script>');
+document.write('<script type="text/javascript" src="js/swiper.js"></script>');
+document.write('<script type="text/javascript" src="js/validation.js"></script>');
