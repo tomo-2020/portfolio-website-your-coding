6fa619525176ec28b332ae968dbb3620b55cd242
tomo-2020 <shige5th2008@yahoo.co.jp>
2020-11-05 12:41:58 +0900

    ＜追加＞formとGoogleフォームを連携
    formとGoogleフォームを連携し、自動返信機能も実装

---
+++ js/form-disabled.js
@@ -0,0 +1,12 @@
+//フォーム form-disabled　必須項目を全て入力するまで送信ボタンを押せないようにする
+$(document).ready(function () {
+    const $submitBtn = $('#js-submit');
+    $('#js-form input,#js-form textarea').on('change', function () {
+        if ($('#js-form input[type="text"]').val() !== '' && $('#js-form input[type="email"]').val() !== '' && $('#js-form input[type="checkbox"]').val() !== '' && $('#js-form #js-agree').prop('checked') === true) {
+            $submitBtn.prop('disabled', false);
+        } else {
+            $submitBtn.prop('disabled', true);
+            //「#js-submit」にもあらかじめ「disabled」を付与しておく
+        }
+    });
+});
--- js/import.js
+++ js/import.js
@@ -1,4 +1,6 @@
 document.write('<script type="text/javascript" src="js/accordion.js"></script>');
+document.write('<script type="text/javascript" src="js/form-disabled.js"></script>');
+document.write('<script type="text/javascript" src="js/google-form.js"></script>');
 document.write('<script type="text/javascript" src="js/hamburger.js"></script>');
 document.write('<script type="text/javascript" src="js/scroll-top.js"></script>');
 document.write('<script type="text/javascript" src="js/smooth-scroll.js"></script>');
